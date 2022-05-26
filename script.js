b076a577b01f7734407ebfcc8377ed7220db7a21
João Victor <jvfaprincipal@gmail.com>
2022-05-26 15:01:20 -0300

    usando gtag fica mais facil

--- script.js
+++ script.js
@@ -1,46 +1,43 @@
-function finalizar(valor) {
-  dataLayer.push({ ecommerce: null });
-  dataLayer.push({
-    event: "purchase",
-    ecommerce: {
-      transaction_id: String((Math.random() * 1000).toFixed()),
-      value: "59.89",
-      currency: "EUR",
-      items: [
-        {
-          item_name: "Triblend Android T-Shirt",
-          item_id: "12345",
-        },
-        {
-          item_name: "Donut Friday Scented T-Shirt",
-          item_id: "67890",
-        },
-      ],
-    },
-  });
-}
+carrinho = []
 
-function adicionar(){
-  gtag("event", "add_to_cart", {
-    currency: "BR",
-    value: 7.77,
+function finalizar(valor) {
+  gtag("event", "purchase", {
+    currency: "USD",
+    transaction_id: String((Math.random() * 1000).toFixed()),
+    value: 21.09,
     items: [
       {
         item_id: "SKU_12345",
         item_name: "Stan and Friends Tee",
+      },
+      {
+        item_id: "SKU_12345",
+        item_name: "Stan and Friends Tee",
+      }
+    ],
+  });
+}
+
+function adicionar(valor, nome){
+  gtag("event", "add_to_cart", {
+    currency: "USD",
+    value: valor,
+    items: [
+      {
+        item_name: String(nome),
       }
     ]
   });
 }
 
-function tirar(){
+function tirar(valor, nome){
   gtag("event", "remove_from_cart", {
     currency: "USD",
     value: 7.77,
+    value: valor,
     items: [
       {
-        item_id: "SKU_12345",
-        item_name: "Stan and Friends Tee",
+        item_name: String(nome),
       }
     ]
   });
